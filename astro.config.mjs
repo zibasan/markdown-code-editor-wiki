7ff55951a592174c5f2fa7c6c5f59c7611b69455
じば <ZIBA3496@gmail.com>
2026-03-20 17:32:39 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,57 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Markdown Editor Docs",
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "日本語",
+          lang: "ja",
+        },
+        en: {
+          label: "English",
+          lang: "en",
+        },
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Getting Started",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Home", link: "/" },
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+        {
+          label: "test-pages",
+          autogenerate: { directory: "test-pages" },
+        },
+      ],
+    }),
+    react(),
+  ],
+});
